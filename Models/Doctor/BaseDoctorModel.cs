67119b939f69f7e2eaf6c7a0a8c2872aed326ff6
Omar Eldriny <omar.m.eldriny@gmail.com>
2025-05-31 21:40:01 +0300

    .

--- Models/Doctor/BaseDoctorModel.cs
+++ Models/Doctor/BaseDoctorModel.cs
@@ -2,6 +2,7 @@
 {
     public abstract class BaseDoctorModel
     {
+        public string FullName { get; set; }
         public string Phone_Number { get; set; }
         public string Specialty { get; set; }
         public string Schdule { get; set; }
